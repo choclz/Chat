ffe9f1a9eb0e5fe70dae9aefae476eddb7f91c0e
Даниил Тихонов <boomboomfak@gmail.com>
2022-04-13 16:14:47 +0300

    Смена контекста

--- DataBase.Context.cs
+++ DataBase.Context.cs
@@ -15,11 +15,18 @@ namespace ClientChat
     
     public partial class MessengerEntities : DbContext
     {
+        private static MessengerEntities _context;
         public MessengerEntities()
             : base("name=MessengerEntities")
         {
         }
-    
+
+        public static MessengerEntities GetContext()
+        {
+            if (_context == null) _context = new MessengerEntities();
+            return _context;
+        }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             throw new UnintentionalCodeFirstException();
